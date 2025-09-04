093a5f8183f1bea1f0f9147b75a75cafe4f50b92
KuKaH <kukah99@gmail.com>
2025-09-04 13:05:43 +0900

    Initial Commit

---
+++ SimHae/SimHaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimHaeApp.swift
+//  SimHae
+//
+//  Created by 홍준범 on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SimHaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimHaeTests/SimHaeTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimHaeTests.swift
+//  SimHaeTests
+//
+//  Created by 홍준범 on 9/4/25.
+//
+
+import Testing
+
+struct SimHaeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
