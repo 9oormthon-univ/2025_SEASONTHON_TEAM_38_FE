9d4fce568af5ddea0d28e54e7fc9dc997b3b1d6d
KuKaH <kukah99@gmail.com>
2025-09-04 13:25:18 +0900

    탭바 화면 세개 생성

---
+++ SimHae/AnalyzeView.swift
@@ -0,0 +1,18 @@
+//
+//  AnalyzeView.swift
+//  SimHae
+//
+//  Created by 홍준범 on 9/4/25.
+//
+
+import SwiftUI
+
+struct AnalyzeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalyzeView()
+}
---
+++ SimHae/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SimHae
+//
+//  Created by 홍준범 on 9/4/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
