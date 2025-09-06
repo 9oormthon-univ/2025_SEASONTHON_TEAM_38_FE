1d2e3d16d5a8365804bb121a11b4eebeec493b43
KuKaH <kukah99@gmail.com>
2025-09-07 06:38:29 +0900

    홈뷰 ui 수정

--- SimHae/HomeView.swift
+++ SimHae/HomeView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-            Image("HomeBackgroundImage")
+        
+        VStack {
+            Image(.appLogo)
                 .resizable()
-                .scaledToFill()
-                .ignoresSafeArea(edges: .top)
-            VStack {
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 18)
+                .padding(.top, 24)
                 Spacer()
                 Image("jellyCha")
                     .resizable()
@@ -22,7 +23,12 @@ struct HomeView: View {
                     .frame(width: 200, height: 200)
                     .padding(.top, 160)
                 Spacer()
-            }
+        }
+        .background {
+            Image("HomeBackgroundImage")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea(edges: .top)
         }
     }
 }
