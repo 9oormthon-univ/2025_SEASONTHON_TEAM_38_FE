8ddf17d699241ab881785f169b5fccf6050af7ee
KuKaH <kukah99@gmail.com>
2025-09-07 06:38:18 +0900

    새로운 탭바 제작 및 네비게이션 스택 구조

--- SimHae/ContentView.swift
+++ SimHae/ContentView.swift
@@ -11,16 +11,143 @@ struct ContentView: View {
     @State private var showingAdd = false
     @StateObject private var calendarVM = CalendarViewModel(service: RealCalendarDreamService())
     
-    @State private var tabSelection: TabBarItem = .home
+    @State private var sessionVM: DreamSessionViewModel?
+    
+    @State private var tabSelection: TabBarItem = .calendar
+    
+    @EnvironmentObject var route: NavigationRouter
+    
     var body: some View {
-        FloatingTabContainerView(selection: $tabSelection, calendarViewModel: calendarVM) {
-            HomeView()
-                .tabBarItem(tab: .home, selection: $tabSelection)
-            CalendarTotalView(calendarViewModel: calendarVM)
-                .tabBarItem(tab: .calendar, selection: $tabSelection)
-            AnalyzeView()
-                .tabBarItem(tab: .analisys, selection: $tabSelection)
+        TabView(selection: $tabSelection) {
+            if tabSelection == .home {
+                HomeView()
+                    .ignoresSafeArea(.keyboard)
+                    .navigationBarBackButtonHidden()
+            }
+            else if tabSelection == .analysis {
+                AnalyzeView()
+                    .ignoresSafeArea(.keyboard)
+                    .navigationBarBackButtonHidden()
+            }
+            else if tabSelection == .calendar {
+                CalendarTotalView(calendarViewModel: calendarVM)
+                    .ignoresSafeArea(.keyboard)
+                    .navigationBarBackButtonHidden()
+            }
         }
+        .overlay {
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    HStack {
+                        Image(systemName: "house.fill")
+                            .frame(width: 64, height: 64)
+                            .font(.system(size: 22))
+                            .background(
+                                Circle()
+                                    .fill(tabSelection == .home ? Color(hex: "#843CFF") : Color(hex: "#FFFFFF").opacity(0.6))
+                                    .overlay(Circle().stroke(Color.white.opacity(0.4), lineWidth: 1))
+                            )
+                            .foregroundStyle(tabSelection == .home ? .white : Color(hex: "#843CFF"))
+                            .frame(width: 64, height: 64)
+                            .foregroundStyle(.red)
+                            .onTapGesture {
+                                withAnimation(.easeInOut(duration: 0.25)) {
+                                    tabSelection = .home
+                                }
+                            }
+                        Image(systemName: "calendar")
+                            .frame(width: 64, height: 64)
+                            .font(.system(size: 22))
+                            .background(
+                                Circle()
+                                    .fill(tabSelection == .calendar ? Color(hex: "#843CFF") : Color(hex: "#FFFFFF").opacity(0.6))
+                                    .overlay(Circle().stroke(Color.white.opacity(0.4), lineWidth: 1))
+                            )
+                            .foregroundStyle(tabSelection == .calendar ? .white : Color(hex: "#843CFF"))
+                            .frame(width: 64, height: 64)
+                            .foregroundStyle(.red)
+                            .onTapGesture {
+                                withAnimation(.easeInOut(duration: 0.25)) {
+                                    tabSelection = .calendar
+                                }
+                            }
+                        Image(systemName: "cloud")
+                            .frame(width: 64, height: 64)
+                            .font(.system(size: 22))
+                            .background(
+                                Circle()
+                                    .fill(tabSelection == .analysis ? Color(hex: "#843CFF") : Color(hex: "#FFFFFF").opacity(0.6))
+                                    .overlay(Circle().stroke(Color.white.opacity(0.4), lineWidth: 1))
+                                
+                            )
+                            .foregroundStyle(tabSelection == .analysis ? .white : Color(hex: "#843CFF"))
+                                    
+                            .frame(width: 64, height: 64)
+                            .foregroundStyle(.red)
+                            .onTapGesture {
+                                withAnimation(.easeInOut(duration: 0.25)) {
+                                    tabSelection = .analysis
+                                        
+                                }
+                            }
+                    }
+                    .padding(12)
+                    .background(
+                        RoundedRectangle(cornerRadius: 100)
+                            .fill(Color(hex: "FFFFFF").opacity(0.2))
+                            .stroke(
+                                LinearGradient(
+                                    gradient: Gradient(colors: [
+                                        Color(hex: "#E8D9FF"),
+                                        Color(hex: "#7534E4"),
+                                        Color(hex: "#E8D9FF")
+                                    ]),
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing
+                                ),
+                                lineWidth: 0.7
+                            )
+                    )
+                    
+                    Spacer()
+                    
+                    Image(systemName: "plus")
+                        .frame(width: 70, height: 70)
+                        .font(.system(size: 28, weight: .light))
+                        .foregroundStyle(.white)
+                        .background(
+                            Circle()
+                                .fill(Color.black)
+                                .overlay(
+                                    Circle()
+                                        .fill(Color(hex: "#843CFF").opacity(0.7)))
+                        )
+                        .overlay(
+                            Circle()
+                                .stroke(
+                                    LinearGradient(
+                                        gradient: Gradient(colors: [
+                                            Color(hex: "#E8D9FF"),
+                                            Color(hex: "#7534E4"),
+                                            Color(hex: "#E8D9FF")
+                                        ]),
+                                        startPoint: .topLeading,
+                                        endPoint: .bottomTrailing
+                                    ),
+                                    lineWidth: 0.7
+                                )
+                        )
+                        .onTapGesture {
+                            route.push(to: .add)
+                        }
+                    Spacer()
+                }
+            }
+        }
+        .toolbarVisibility(.hidden, for: .tabBar)
+        .ignoresSafeArea(.keyboard)
     }
 }
 
--- SimHae/SimHaeApp.swift
+++ SimHae/SimHaeApp.swift
@@ -10,29 +10,80 @@ import Combine
 
 @main
 struct SimHaeApp: App {
+    
     @State private var bag = Set<AnyCancellable>()
     private let auth = AuthService()
     @AppStorage("didRegisterAnonymous") private var didRegisterAnonymous = false
-
+    @StateObject private var DreamSessionVM = DreamSessionViewModel(service: RealDreamService(), speech: SpeechInputViewModel(speechRecognizer: SpeechRecognizer()))
+    @StateObject private var calendarVM = CalendarViewModel(service: RealCalendarDreamService())
+    
+    @ObservedObject private var route: NavigationRouter = NavigationRouter()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .preferredColorScheme(.dark)
-                .task {
-                    // guard !didRegisterAnonymous else { return }  // 임시로 주석! 실제로는 이 코드로 한번만 하면 됨!
-                    auth.ensureAnonymousUser()
-                        .sink { completion in
-                            switch completion {
-                            case .finished:
-                                didRegisterAnonymous = true
-                            case .failure(let error):
-                                print("❌ 요청 실패:", error)
+            NavigationStack(path: $route.path,
+                            root: {
+                ContentView()
+                    .preferredColorScheme(.dark)
+                    .task {
+                        // guard !didRegisterAnonymous else { return }  // 임시로 주석! 실제로는 이 코드로 한번만 하면 됨!
+                        auth.ensureAnonymousUser()
+                            .sink { completion in
+                                switch completion {
+                                case .finished:
+                                    didRegisterAnonymous = true
+                                case .failure(let error):
+                                    print("❌ 요청 실패:", error)
+                                }
+                            } receiveValue: { resp in
+                                print("(서버가 잘 응답함) 🆔 내 UUID:", AnonymousId.getOrCreate())
                             }
-                        } receiveValue: { resp in
-                            print("(서버가 잘 응답함) 🆔 내 UUID:", AnonymousId.getOrCreate())
+                            .store(in: &bag)
+                    }
+                    .ignoresSafeArea(.keyboard)
+                    .environmentObject(route)
+                    .environmentObject(calendarVM)
+                    .navigationDestination(for: RouteType.self,
+                                           destination: { type in
+                        switch type {
+                        case .add: AddDreamView(vm: DreamSessionVM, selectedDate: $calendarVM.selectDate)
+                                .environmentObject(route)
+                                .environmentObject(calendarVM)
+                       // case .add: AddDreamView(vm: DreamSessionVM, selectedDate: $calendarVM.selectDate, calendarViewModel: calendarVM)
+                                //.environmentObject(route)
+                        case .loading: DreamLoadingView(vm: DreamSessionVM)
+                                .environmentObject(route)
+                        case .summary: DreamSummaryView(vm: DreamSessionVM)
+                                .environmentObject(route)
+                        case .interpretation: DreamInterpretationView(vm: DreamSessionVM)
+                                .environmentObject(route)
+                        case .suggestion: DreamSuggestionView(vm: DreamSessionVM).environmentObject(route)
+                                .environmentObject(calendarVM)
                         }
-                        .store(in: &bag)
-                }
+                    })
+                
+            })
+                
         }
+    }
+}
+
+enum RouteType: Hashable {
+    case add, loading, summary, interpretation, suggestion
+}
+
+class NavigationRouter: ObservableObject {
+    @Published var path = NavigationPath()
+    
+    func push(to route: RouteType) {
+        path.append(route)
+    }
+    
+    func pop() {
+        path.removeLast()
+    }
+    
+    func removeAll() {
+        path.removeLast(path.count)
     }
 }
