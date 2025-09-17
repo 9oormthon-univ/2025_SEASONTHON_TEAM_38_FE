3ca57ddce741a54165ebe8c2369e8bb49f1aad71
KuKaH <kukah99@gmail.com>
2025-09-18 06:36:42 +0900

    로그인 기능 연동

--- SimHae/SimHaeApp.swift
+++ SimHae/SimHaeApp.swift
@@ -14,9 +14,10 @@ struct SimHaeApp: App {
         NotificationManager.shared.configure()
     }
     
+    @StateObject private var authVM = AuthViewModel()
+    
     @State private var bag = Set<AnyCancellable>()
     private let auth = AuthService()
-    @AppStorage("didRegisterAnonymous") private var didRegisterAnonymous = false
     @StateObject private var DreamSessionVM = DreamSessionViewModel(service: RealDreamService(), speech: SpeechInputViewModel(speechRecognizer: SpeechRecognizer()))
     @StateObject private var calendarVM = CalendarViewModel(service: RealCalendarDreamService())
     
@@ -24,50 +25,42 @@ struct SimHaeApp: App {
     
     var body: some Scene {
         WindowGroup {
-            NavigationStack(path: $route.path,
-                            root: {
-                ContentView()
-                    .preferredColorScheme(.dark)
-                    .task {
-                        // guard !didRegisterAnonymous else { return }  // 임시로 주석! 실제로는 이 코드로 한번만 하면 됨!
-                        auth.ensureAnonymousUser()
-                            .sink { completion in
-                                switch completion {
-                                case .finished:
-                                    didRegisterAnonymous = true
-                                case .failure(let error):
-                                    print("❌ 요청 실패:", error)
+            Group {
+                if authVM.isAuthenticated {
+                    NavigationStack(path: $route.path,
+                                    root: {
+                        ContentView()
+                            .environmentObject(authVM)
+                            .preferredColorScheme(.dark)
+                            .ignoresSafeArea(.keyboard)
+                            .environmentObject(route)
+                            .environmentObject(calendarVM)
+                            .navigationDestination(for: RouteType.self,
+                                                   destination: { type in
+                                switch type {
+                                case .add: AddDreamView(vm: DreamSessionVM, selectedDate: $calendarVM.selectDate)
+                                        .environmentObject(route)
+                                        .environmentObject(calendarVM)
+                                case .loading: DreamLoadingView(vm: DreamSessionVM)
+                                        .environmentObject(route)
+                                case .summary: DreamSummaryView(vm: DreamSessionVM)
+                                        .environmentObject(route)
+                                        .environmentObject(calendarVM)
+                                case .interpretation: DreamInterpretationView(vm: DreamSessionVM)
+                                        .environmentObject(route)
+                                        .environmentObject(calendarVM)
+                                case .suggestion: DreamSuggestionView(vm: DreamSessionVM)
+                                        .environmentObject(route)
+                                        .environmentObject(calendarVM)
                                 }
-                            } receiveValue: { resp in
-                                print("(서버가 잘 응답함) 🆔 내 UUID:", AnonymousId.getOrCreate())
-                            }
-                            .store(in: &bag)
-                    }
-                    .ignoresSafeArea(.keyboard)
-                    .environmentObject(route)
-                    .environmentObject(calendarVM)
-                    .navigationDestination(for: RouteType.self,
-                                           destination: { type in
-                        switch type {
-                        case .add: AddDreamView(vm: DreamSessionVM, selectedDate: $calendarVM.selectDate)
-                                .environmentObject(route)
-                                .environmentObject(calendarVM)
-                        case .loading: DreamLoadingView(vm: DreamSessionVM)
-                                .environmentObject(route)
-                        case .summary: DreamSummaryView(vm: DreamSessionVM)
-                                .environmentObject(route)
-                                .environmentObject(calendarVM)
-                        case .interpretation: DreamInterpretationView(vm: DreamSessionVM)
-                                .environmentObject(route)
-                                .environmentObject(calendarVM)
-                        case .suggestion: DreamSuggestionView(vm: DreamSessionVM)
-                                .environmentObject(route)
-                                .environmentObject(calendarVM)
-                        }
+                            })
+                        
                     })
-                
-            })
-                
+                } else {
+                    LoginView()
+                        .environmentObject(authVM)
+                }
+            }
         }
     }
 }
